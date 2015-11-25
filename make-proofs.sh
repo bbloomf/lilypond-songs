7f9f8f88f66b03d368f768e9fc951c44745f3f36
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2015-11-25 12:00:29 -0500

    Obviated need for marksinfo.txt and pdftk

--- make-proofs.sh
+++ make-proofs.sh
@@ -8,21 +8,12 @@ popd
 echo 'Updating lilypond-songs repository'
 if git pull
 then
-  cp \!full.pdf gh-pages/PDFs/sftpd.pdf
   cp \!full.pdf 9781484022801_content.pdf
-  echo 'Extracting odd pages.'
-  pdftk A=\!full.pdf cat Aodd output odd.pdf
-  echo 'Extracting even pages.'
-  pdftk A=\!full.pdf cat Aeven output even.pdf
-  gswin64c -o \!odd.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [64.8 18 565.2 774] /PAGES pdfmark" -f odd.pdf
-  gswin64c -o \!even.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "[/CropBox [46.8 18 547.2 774] /PAGES pdfmark" -f even.pdf
 
-  pdftk A=\!odd.pdf B=\!even.pdf shuffle A B output \!sftpd-playbook.pdf
-  pdftk \!sftpd-playbook.pdf dump_data_utf8 output data.txt
-  # put the marksinfo.txt file right after the "NumberOfPages" line.
-  sed -e '0,/NumberOfPages:/{//r ly/Contents/marksinfo.txt
-}' data.txt > newdata.txt
-  pdftk \!sftpd-playbook.pdf update_info_utf8 newdata.txt output gh-pages/PDFs/sftpd-playbook.pdf
+  echo 'Adding bookmarks to 8.5x11 version'
+  gs -o gh-pages/PDFs/sftpd.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -f \!full.pdf pdfmarks.txt
+  echo 'Making tablet version'
+  gs -o gh-pages/PDFs/sftpd-playbook.pdf -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -c "<</EndPage {0 eq {2 mod 0 eq {[/CropBox [64.8 18 565.2 774] /PAGE pdfmark true} {[/CropBox [46.8 18 547.2 774] /PAGE pdfmark true} ifelse}{false}ifelse}>> setpagedevice" -f \!full.pdf pdfmarks.txt
 
   pushd gh-pages
   if git add PDFs/sftpd-playbook.pdf PDFs/sftpd.pdf
@@ -42,7 +33,6 @@ then
     echo 'Error from git add --^'
   fi
   popd
-  rm even.pdf odd.pdf \!even.pdf \!odd.pdf \!sftpd-playbook.pdf data.txt newdata.txt
 else
   echo 'Error from git --^'
 fi
